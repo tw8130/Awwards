4de134eb573c4a4d003298f89868676796991b02
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-06-06 22:48:06 +0300

    add a newsletter model with email and name of subscriber

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 from django.db.models.signals import post_save 
 from django.dispatch import receiver
+import numpy as np
+from django.db.models import Avg, Max, Min
 # Create your models here.
 
 class Profile(models.Model):
@@ -10,7 +12,6 @@ class Profile(models.Model):
     prof_user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
     bio = models.TextField()
     contact_info = models.CharField(max_length=200,blank=True)
-    all_projects = models.ForeignKey(Project,on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return f'{self.user.username}.Profile'
@@ -76,3 +77,7 @@ class Review(models.Model):
 
     def __str__(self):
         return self.comment
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
