37ba67f21cdb7d3f1bc4ea1d07e400cf8628697c
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-06-07 22:23:52 +0300

    import our forms from our forms module then create a register view function

--- posts/views.py
+++ posts/views.py
@@ -2,11 +2,26 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .models import Project,Profile,Review
-from .forms import NewsLetterForm
+from .forms import NewsLetterForm,RegistrationForm,ReviewForm,ProfileForm,ProjectForm
+from django.contrib import messages
 from .email import send_welcome_email
 from django.http import JsonResponse
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Thank you, {username}. Your account has been created')
+            return redirect('login')
+
+    else:
+        form = RegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
 
 @login_required(login_url='/accounts/login/')
 def welcome(request):
@@ -73,3 +88,56 @@ def project(request, id):
                                           'comments':comments,
                                           'latest_review_list':latest_review_list})
 
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    projects = Project.objects.filter(user = current_user)
+
+    try:   
+        prof = Profile.objects.get(prof_user=current_user)
+    except ObjectDoesNotExist:
+        return redirect('new_profile')
+
+    return render(request,'profile.html',{'profile':prof,'projects':projects})   
+
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.prof_user = current_user
+            profile.profile_Id = request.user.id
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'new_profile.html', {"form": form})   
+
+@login_required(login_url='/accounts/login/')
+def profile_edit(request):
+    current_user = request.user
+    if request.method == 'POST':
+        logged_user = Profile.objects.get(prof_user=request.user)
+        form = ProfileForm(request.POST, request.FILES, instance=logged_user)
+        if form.is_valid():
+            form.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request,'edit_profile.html',{'form':form})
+
+@login_required(login_url='/accounts/login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectForm(request.POST,request.FILES)
+        if form.is_valid():
+            user_image = form.save(commit=False)
+            user_image.user = current_user
+            user_image.save()
+        return redirect('index')
+    else:
+        form = ProjectForm()
+    return render(request,"new_project.html",{"form":form})
