64809ce27e2643980ebda5600b8aab5f35b37921
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-06-07 22:34:46 +0300

    we import Usercreationform from auth.forms module also import our models then create our registration form

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile,Review,Project
 
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(label="Email Address")
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
