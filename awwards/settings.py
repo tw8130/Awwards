970d4775ba810a0736235096d577c94aff9659e0
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-06-08 12:26:03 +0300

    add 'rest_framework.authtoken' to our INSTALLED_APPS setting then define the DEFAULT_AUTHENTICATION_CLASSES for the rest_frameworkpackage

--- awwards/settings.py
+++ awwards/settings.py
@@ -41,7 +41,14 @@ INSTALLED_APPS = [
     'posts.apps.PostsConfig',
     'bootstrap3',
     'tinymce',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
