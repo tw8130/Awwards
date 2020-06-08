fda5f1b3f34b8f28ed376419116051f86fde8b04
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-06-08 15:49:56 +0300

    import the project settings from the django.conf module and a static function. We then add to the urlpatterns a new static route that references the location to the uploaded files

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.welcome, name='index'),
     path('ajax/newsletter/', views.newsletter, name='newsletter'),
+    path('api/project/', views.ProjectList.as_view()),
+    path('api/profile/', views.ProfileList.as_view()),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password_reset/',
@@ -19,4 +24,7 @@ urlpatterns = [
     path('password_reset_complete/',
           auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
           name='password_reset_complete'),
+      
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
